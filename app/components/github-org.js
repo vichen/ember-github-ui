d18ea6988203f43f25837cc5f9df7a12b8d6c9c3
Vivian Chen <vivianchen@Vivians-MacBook-Pro.local>
2016-10-05 10:21:36 -0700

    exercise 7

--- app/components/github-org.js
+++ app/components/github-org.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
+import isInArray from 'github-ui/utils/is-in-array';
 
 export default Ember.Component.extend({
   tagName: 'li',
-
+  favorites: Ember.inject.service(),
+  isFavorited: isInArray('org', 'favorites.items'),
   action: {
-    favoriteWasClicked() {
+    favoriteClicked() {
       this.sendAction('on-fav-clicked', this.get('org'))
     }
   }
